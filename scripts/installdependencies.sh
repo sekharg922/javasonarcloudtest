ba4705b3d16973d21121f8a503982854049cd5cc
Raj Goli <rgoli@cryoport.com>
2020-03-03 14:39:47 -0800

    adding some appsepc.yml file

--- scripts/installdependencies.sh
+++ scripts/installdependencies.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 #sudo amazon-linux-extras enable corretto8
-sudo yum -y install java-1.8.0-amazon-corretto
+sudo rpm --import https://yum.corretto.aws/corretto.key 
+sudo curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
+sudo yum install -y java-1.8.0-amazon-corretto-devel
