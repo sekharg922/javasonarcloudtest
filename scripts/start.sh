e005e45bfb91dc530f272f2d8cb6770115b2c4a2
Raj Goli <rgoli@cryoport.com>
2020-03-03 16:22:41 -0800

    adding some appsepc.yml file

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+#start the Java service on port 8081
 cd /home/ec2-user/server
-sudo /usr/bin/java -jar -Dserver.port=80 \
+sudo /usr/bin/java -jar -Dserver.port=8081 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
+
+
+#start the react JS  app on port 8080
+cd /home/ec2-user/web
+sudo npm install
+sudo npm start
