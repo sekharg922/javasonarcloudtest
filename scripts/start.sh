43ffbe6a18f07c5c4c9aed632fcbc4e8b0fc6187
Raj Goli <rgoli@cryoport.com>
2020-03-06 10:12:59 -0800

    start sh test

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@
 #start the Java service on port 8081
 cd /home/ec2-user/server
 sudo /usr/bin/java -jar -Dserver.port=8081 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+    target/*.jar > /dev/null 2> /dev/null < /dev/null &
 
 
 #start the react JS  app on port 8080
